9c21f3f5260a26b6ea02e483a94a7091073fcd7c
Ewa <bittner.ewa@gmail.com>
2024-11-29 14:36:31 +0100

    workspaces

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "rg_name" {
+  type = string
+  description = "Name of the resource group"
+}
+
+variable "rg_location" {
+  type = string
+  description = "Location og the resource group"
+}
